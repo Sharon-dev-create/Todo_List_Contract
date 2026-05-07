413909f9575808518bf0d0b927492a17a8326f1a
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-05-07 16:21:33 +0100

    writing tests

---
+++ test/TodoListTest.t.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {TodoList} from "../contracts/TodoList.sol";
+
+contract TodoListTest is Test {
+    
+}
